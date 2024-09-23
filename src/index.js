28c1719e876b7490d8e53c442b4b0485365ff709
main73 <mainuddinrusselbuet73@yahoo.com>
2024-09-23 23:44:49 +0600

    uiuiasdf

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Router>
+     <Router  basename={process.env.PUBLIC_URL}>
      <App />
      </Router>
   </React.StrictMode>
